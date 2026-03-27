b9cd46242270bd03d6d1b86924973d62104ce88a
dsamborschi <dsamborschi@gmail.com>
2026-03-26 23:58:02 -0400

    refac

--- agent/src/api/middleware/index.ts
+++ agent/src/api/middleware/index.ts
@@ -4,5 +4,5 @@
 
 export { default as logging, setLogger as setLoggingLogger } from './logging';
 export { default as auth } from './auth';
-export { default as networkSecurity, setNetworkSecurityLogger } from './network-security';
+export { default as networkSecurity, setNetworkSecurityLogger } from './network';
 export { default as errors, setLogger as setErrorsLogger } from './errors';
